54eb1d6f7d8e27a3d9066c6b6b6a576668a5468a
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:57:50 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "healthcare_medtech_service_fhir_destinations" {
+  description = "All healthcare_medtech_service_fhir_destination resources"
+  value       = azurerm_healthcare_medtech_service_fhir_destination.healthcare_medtech_service_fhir_destinations
+}
+
